d5b7e9cbca9d5637b348d447967323829550b062
Md. Saif Islam <33100614+saifDiu@users.noreply.github.com>
2020-09-17 13:06:25 +0600

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alsaif839@gmail.com'
-EMAIL_HOST_PASSWORD = '21081998saif'
+EMAIL_HOST_USER = '######'
+EMAIL_HOST_PASSWORD = '########'
